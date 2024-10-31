e15f268361eb218e5501fd887a49921ec4d7fa42
DESKTOP-I0DCTD9\admin <a>
2024-10-31 22:57:48 +0800

    update : 1、added Ethereum structure abstraction and Ethereum client initialization logic. 2、 added alloy and tokio related dependencies.

--- happypto-rs/src/chains.rs
+++ happypto-rs/src/chains.rs
@@ -2,18 +2,58 @@ pub enum Chain {
     Ethereum,
     Solana,
     Aptos,
+    Sui,
 }
 
-pub struct Chain {
-    chain: Chain,
+use alloy::{
+    providers::{Provider, ProviderBuilder, RootProvider},
+    transports::http::{Client, Http},
+};
+use eyre::{Ok, Result};
+/// Ethereum network abstraction
+pub struct Ethereum {
+    provider: RootProvider<Http<Client>>,
 }
-
-impl Chain {
-    pub fn lastBlock() -> u64 {
-        
+impl Ethereum {
+    pub fn new(rpc: &str) -> Result<Ethereum> {
+        Ok(Self {
+            provider: ProviderBuilder::new().on_http(rpc.trim().parse()?),
+        })
+    }
+    pub async fn address_balance() -> Result<u64> {
+        Ok(0)
+    }
+    pub async fn last_block(&self) -> Result<u64> {
+        Ok(self.provider.get_block_number().await?)
+    }
+    pub fn reset_provider(&mut self, rpc: &str) -> Result<()> {
+        self.provider = ProviderBuilder::new().on_http(rpc.trim().parse()?);
+        Ok(())
     }
 }
 
-pub struct Ethereum {}
-pub struct Solana {}
-pub struct Aptos {}
+/// Solana network abstraction
+pub struct Solana {
+    rpc: String,
+}
+impl Solana {
+    pub fn new(rpc: String) -> Self {
+        Self { rpc: rpc }
+    }
+    pub fn last_block(&self) -> u64 {
+        0
+    }
+}
+
+/// Aptos network abstraction
+pub struct Aptos {
+    rpc: String,
+}
+impl Aptos {
+    pub fn new(rpc: String) -> Self {
+        Self { rpc: rpc }
+    }
+    pub fn last_block(&self) -> u64 {
+        0
+    }
+}
