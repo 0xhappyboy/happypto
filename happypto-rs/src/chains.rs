c919d8918f0652f93200bc3877f74caac5202dae
DESKTOP-I0DCTD9\admin <a>
2024-10-31 20:52:34 +0800

    update : project init

---
+++ happypto-rs/src/chains.rs
@@ -0,0 +1,19 @@
+pub enum Chain {
+    Ethereum,
+    Solana,
+    Aptos,
+}
+
+pub struct Chain {
+    chain: Chain,
+}
+
+impl Chain {
+    pub fn lastBlock() -> u64 {
+        
+    }
+}
+
+pub struct Ethereum {}
+pub struct Solana {}
+pub struct Aptos {}
