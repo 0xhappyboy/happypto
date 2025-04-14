ebf21b78a65815feb8d1e34b3d50b2876ff6275a
0xhappyboy <superhappyboy1995@gmail.com>
2025-04-15 07:34:20 +0800

    update : added the function of creating a new wallet cluster

--- happypto_solana/src/wallet.rs
+++ happypto_solana/src/wallet.rs
@@ -21,4 +21,20 @@ impl Wallet {
             public_key: k.pubkey().to_string(),
         })
     }
+    /// create new wallet cluster
+    pub fn new_wallet_cluster(amount: u64) -> Result<Vec<Self>> {
+        //let mut wallets = vec![];
+        let mut wallets: Map<String, Wallet> = HashMap::new();
+        for i in 0..amount {
+            loop {
+                let w = Wallet::new();
+                if wallets.entry(w.unwrap().private_key) {
+                    wallets.insert(w.unwrap().private_key, w.unwrap());
+                    break;
+                }
+            }
+        }
+    }
+    /// create new wallet cluster by private key
+    pub fn new_wallet_cluster_private_keys(pks: Vec<String>) -> Result<Vec<Self>> {}
 }
