acd7b9f17427cdaf327b0f9c0e7f2b8e22824cb7
0xhappyboy <superhappyboy1995@gmail.com>
2025-04-27 06:01:36 +0800

    update : added support for hyperliquid networks

---
+++ happypto_hyperliquid/src/config.rs
@@ -0,0 +1,8 @@
+//! overall situation config
+use std::str::FromStr;
+
+use once_cell::sync::Lazy;
+use url::Url;
+
+pub const HYPERLIQUID_TEST_NET_URL: &'static str = "https://api.hyperliquid.xyz";
+pub const HYPERLIQUID_NET_URL: &'static str = "https://api.hyperliquid-testnet.xyz";
